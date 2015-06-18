2a0cc84969c45f7dfb175da83d77ecd5ef4cb5aa
Joy Kuang <q-cute@163.com>
2015-06-18 18:05:12 +0800

    项目正式发行 版本1.0.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Plugins for front-end programming
+一些前端小插件，不定期更新
+
+## Plugins List
+1.**autoHeight 自动高度**
+
+## Dependences
+1.**node.js**
+2.**uglifyjs** [node]
+
+## Todos
+* Code optimized
+
+
+## Copyright and License
+
+Copyright (c) [悦软工作室](http://www.yueruanstudio.com)  2015 under the [MIT license](LICENSE.md).
---
+++ autoHeight/cm.autoHeight.js
@@ -0,0 +1,59 @@
+/*! 
+  * AutoHeight Plugin, Coded by Joy Kuang, 
+  * https://github.com/joykuang/plugins
+  * http://joykuang.com for more information about me.
+  * @pama element: selector with [data-height]
+  * @pama callback: callback function
+  * @return: element with autoHeighted!
+**/
+var autoHeight = function (element, callback) {
+    //检测Zepto/jQuery依赖，无依赖则抛出错误
+    if (!Zepto && typeof Zepto !== "function") {
+        throw new Error("Zepto/jQuery required!");
+        return;
+    }
+    if (!jQuery && typeof jQuery !== "function") {
+        throw new Error("Zepto/jQuery required!");
+        return;
+    }
+    //正则表达式匹配[两位非零整数:两位非零整数]
+    var expr = /^\+?[1-9][0-9]*:\+?[1-9][0-9]*$/;
+    //判断选择器
+    if (element === Object) {
+        var current = $(element),
+            children = current.find("[data-height]"),
+            divHeight = 0,
+            currentHeight = current.attr("data-height");
+            
+        if (!current.attr("data-height")) {
+            if (expr.test(currentHeight)) {
+                var w = parseInt(currentHeight.split(':')[0]);
+                var h = parseInt(currentHeight.split(':')[1]);
+                divHeight = current.width() / w * h;
+                divDom.height(divHeight)
+            } 
+        }
+        children.each(function () {
+             var current = $(this);
+            if (expr.test(currentHeight)) {
+                var w = parseInt(currentHeight.split(':')[0]);
+                var h = parseInt(currentHeight.split(':')[1]);
+                divHeight = current.width() / w * h;
+                divDom.height(divHeight)
+            } 
+        });
+    } else {
+        $("[data-height]").each(function () {
+            var divDom = $(this);
+            divHeight = 0, value = divDom.attr("data-height");
+            if (expr.test(value)) {
+                var w = parseInt(value.split(':')[0]);
+                var h = parseInt(value.split(':')[1]);
+                divHeight = divDom.width() / w * h;
+                divDom.height(divHeight)
+            } 
+        });
+    }
+    callback && typeof callback === "function" && callback();
+}
+window.autoHeight = autoHeight;
